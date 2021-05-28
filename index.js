c03417a7c95ce68a06f662cf9bcc343af6c2e362
Everardo Jorge <ejorge607@gmail.com>
2021-05-28 00:50:08 -0500

    change port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(5000)
